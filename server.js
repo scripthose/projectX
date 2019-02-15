db252facb378b48df6a32452839cc6a93cc3c2f2
Yassen Higazi <yassenka28@gmail.com>
2019-02-15 06:11:44 +0200

    add logger and some error handling

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
+const logger = require('morgan');
 const express = require("express");
 const bodyParser = require("body-parser");
+const createError = require('http-errors');
 
 const config = require('./config');
 const IndexRoute = require('./routes');
@@ -8,6 +10,9 @@ const app = express();
 
 // for serving static files
 app.use(express.static(config.PUBLIC_URL));
+
+// Logger
+app.use(logger('dev'));
 
 // use bodyParser to parse json and request body
 app.use(bodyParser.json());
@@ -19,6 +24,23 @@ app.set("views", config.PUBLIC_URL);
 
 // Routes
 app.use(IndexRoute);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.status + " " + err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
 
 // start listening...
 app.listen(config.PORT, () =>
