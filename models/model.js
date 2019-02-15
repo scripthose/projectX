90dbe10d4b07b9f6b40e187df786a4d662ce19b9
Yassen Higazi <yassenka28@gmail.com>
2019-02-15 13:29:36 +0200

    format model.js

--- models/model.js
+++ models/model.js
@@ -6,101 +6,105 @@ const Schema = mongoose.Schema;
 //Employee Schem
 
 const EmployeeSchema = new Schema({
-    name:{
-        type:String,
-        required : true,
+    name: {
+        type: String,
+        required: true,
         trim: true
     },
-    username:{
-        type:String,
-        required : true,
+    username: {
+        type: String,
+        required: true,
         trim: true
     },
-    password:{
-        type:String,
-        required : true,
+    password: {
+        type: String,
+        required: true,
         trim: true
     },
-    tel:{
-        type:Number,
-        required : true
+    tel: {
+        type: Number,
+        required: true
     },
-    catchTime:{
-        default:Date.now
+    catchTime: {
+        default: Date.now
     }
 });
 
 //Store Schema
 const StorageSchema = new Schema({
-    address:{
-        type:String,
-        required : true
+    address: {
+        type: String,
+        required: true
     },
-    capacity:{
-        type:Number,
-        required : true
+    capacity: {
+        type: Number,
+        required: true
     },
-    emp_id:{
-        type:String,
-        required:true
+    emp_id: {
+        type: String,
+        required: true
     },
-    prod_id:{
-        type:String
-    }
+    prod_id: [{
+        type: Schema.Types.ObjectId,
+        ref: 'product'
+    }]
 });
 
 //Product Schema
 const ProductSchema = new Schema({
-    name:{
-        type:String,
-        required : true
+    name: {
+        type: String,
+        required: true
     },
-    productionDate:{
-        type:Date,
-        required : true
+    productionDate: {
+        type: Date,
+        required: true
     },
-    expetDate:{
-        type:Date,
-        required : true
+    expirationDate: {
+        type: Date,
+        required: true
     },
-    quentity:{
-        type:Number,
-        required : true
+    quentity: {
+        type: Number,
+        required: true
     },
-    exporter:{
-        type:Number,
-        required : true
+    exporter: {
+        type: Schema.Types.ObjectId,
+        ref: 'exporter',
+        required: true
     },
-    type:{
-        type:String,
-        required : true
+    type: {
+        type: String,
+        required: true
     },
-    price:{
-        type:Number,
-        required : true
+    price: {
+        type: Number,
+        required: true
     },
-    name:{
-        type:String,
-        required : true
+    name: {
+        type: String,
+        required: true
     },
-    stor_id:{
-        type:String,
-        required : true
+    stor_id: {
+        type: Schema.Types.ObjectId,
+        ref: 'storage',
+        required: true
     }
 });
 
-const ExporterSchema  = new Schema({
-    name:{
-        type:String,
-        required : true
+const ExporterSchema = new Schema({
+    name: {
+        type: String,
+        required: true
     },
-    tel:{
-        type:Number,
-        required : true
+    tel: {
+        type: Number,
+        required: true
     },
-    Prod_id:{
-        type:String,
-        required : true
+    Prod_id: {
+        type: Schema.Types.ObjectId,
+        ref: 'product',
+        required: true
     }
 });
 
@@ -109,13 +113,13 @@ Employee.plugin(timestamp);
 const employee = mongoose.model('employee', EmployeeSchema);
 const storage = mongoose.model('storage', StorageSchema);
 const product = mongoose.model('product', ProductSchema);
-const exporter = mongoose.model('scriptors', ExporterSchema);
+const exporter = mongoose.model('exporter', ExporterSchema);
 
 
 // exporting the models outside of the
 module.exports = {
-	employee,
-	storage,
-	product,
-	exporter
+    employee,
+    storage,
+    product,
+    exporter
 }
